1c8464f5b105c262456eca9fc99e786bd200da8b
Mihir Dalvi <53440775+MihirDalvi@users.noreply.github.com>
2024-10-14 21:24:01 +0530

    Create Assignment3.java

---
+++ ADS/Assignments/Assignment 3/Assignment3.java
@@ -0,0 +1,78 @@
+
+class Queue {
+    int size = 5;
+    int cq [] = new int[size];
+    int front, rear;
+
+    Queue (){
+        front =-1;
+        rear = -1;
+
+    }
+    boolean  isEmpty (){
+        return (front == -1);
+
+    }
+    boolean isFull(){
+        return ((rear + 1) % size == front);
+    }
+
+    void enqueue (int ele){
+        if (isFull()) {
+            System.out.println("queue is full");
+            return;
+        }
+        else{
+
+            if (front == -1){
+                front = 0;
+            }
+            rear = (rear + 1) % size;  // Move rear circularly
+            cq[rear] = ele;
+        }
+    }
+
+    int dequeue (){
+        if(isEmpty())
+        {
+            System.out.println("queue is empty");
+            return -1;
+        }
+        else
+        {
+
+            int deletedEle = cq[front];
+            if (front == rear)
+                {
+                    front = -1;
+                    rear = -1;
+                }
+            else
+                {
+                    front = (front+1) % size; //f++
+                }
+                return deletedEle;
+        }
+        
+        }
+        
+
+    }
+
+
+public class Assignment3 {
+    public static void main(String [] args)
+    {
+        Queue q1 = new Queue();
+        System.out.println(q1.isEmpty());
+        
+
+        q1.enqueue(1);
+        q1.enqueue(2);
+        q1.enqueue(3);
+        q1.enqueue(4);
+        q1.enqueue(5);
+        q1.enqueue(6);
+
+    }
+}
